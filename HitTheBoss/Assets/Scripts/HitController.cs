b973dbc4f0a9058da6be4e2d4dc8fb2ed4085be4
AnnCrudelis <ann.crudelis@gmail.com>
2019-12-24 17:28:59 +0600

    no message

---
+++ HitTheBoss/Assets/Scripts/HitController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class HitController : MonoBehaviour
+{
+    public float HP;
+    public SimpleHealthBar healthBar;
+    float current = 0;
+    float max = 0;
+    void Start()
+    {
+
+    }
+
+
+
+    public void DMG(float dmg)
+    {
+        HP -= dmg;
+        if (HP <= 0)
+        {
+            HP = 0;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //healthBar.UpdateBar(current, max);
+    }
+}
